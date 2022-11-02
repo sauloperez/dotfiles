2a0f7d6992df797216e1a79a2be44f0eeab8c438
Pau Perez <saulopefa@gmail.com>
2022-11-02 12:57:40 +0100

    Add old keybinds to find files and buffers with Telescope

--- nvchad_config/custom/mappings.lua
+++ nvchad_config/custom/mappings.lua
@@ -3,6 +3,8 @@ local M = {}
 M.general = {
   n = {
     ["\\g"] = { "<cmd> Telescope grep_string <CR>", "search string under cursor" },
+    ["<space>b"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
+    ["<space><space>"] = { "<cmd> Telescope find_files <CR>", "find files" },
   },
 }
 
