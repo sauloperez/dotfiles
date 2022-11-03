5955d0a63ff07a5fb678f50fa3b5a3ad7ff2bab2
Pau Perez <saulopefa@gmail.com>
2022-11-03 14:22:12 +0100

    Add old git grep Neovim keybinding

--- nvchad_config/custom/mappings.lua
+++ nvchad_config/custom/mappings.lua
@@ -3,6 +3,7 @@ local M = {}
 M.general = {
   n = {
     ["\\g"] = { "<cmd> Telescope grep_string <CR>", "search string under cursor" },
+    [",gg"] = { "<cmd> Telescope live_grep <CR>", "git grep" },
     ["<space>b"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
     ["<space><space>"] = { "<cmd> Telescope find_files <CR>", "find files" },
   },
