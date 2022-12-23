c4fed33f9de730aaf855ec9c57dceb19c74c4926
Pau Perez <saulopefa@gmail.com>
2022-12-23 21:03:13 +0100

    Enable Neovim's incremental search

--- nvchad_config/custom/init.lua
+++ nvchad_config/custom/init.lua
@@ -4,6 +4,8 @@ vim.g.mapleader = ','
 
 vim.cmd("iabbrev rem import remote_pdb; remote_pdb.set_trace(host='0.0.0.0', port=4445)")
 
+vim.opt.incsearch = true
+
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
