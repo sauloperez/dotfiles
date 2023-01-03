8009b9421f7c783ea6cf2ca502dec3af2ba6c0a5
Pau Perez <saulopefa@gmail.com>
2023-01-03 15:32:00 +0100

    Remove LSP formatters I don't need

--- nvchad_config/custom/plugins/null-ls.lua
+++ nvchad_config/custom/plugins/null-ls.lua
@@ -8,8 +8,7 @@ local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt,
+  -- web stuff
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } },
 
   -- Lua
@@ -18,10 +17,6 @@ local sources = {
   -- Shell
   b.formatting.shfmt,
   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
-
-  -- cpp
-  b.formatting.clang_format,
-  b.formatting.rustfmt,
 }
 
 null_ls.setup {
