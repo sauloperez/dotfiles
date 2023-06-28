c097a9be11b1ca8489996ace2eb70b57e127b066
Pau Perez <saulopefa@gmail.com>
2023-06-28 17:28:16 +0200

    Tweak formatting on save with null-ls

--- nvchad_config/custom/configs/null-ls.lua
+++ nvchad_config/custom/configs/null-ls.lua
@@ -1,30 +1,43 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-  return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+	-- webdev stuff
+	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
-  -- Python
-  b.formatting.black,
-  b.diagnostics.ruff,
+	-- Python
+	b.formatting.black,
+	b.diagnostics.ruff,
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- Shell
-  b.formatting.shfmt,
-  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = function(client, bufnr) -- format on save
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
