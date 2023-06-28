f31cb3cdb98502b92e28cbd3f3da1008c1998876
Pau Perez <saulopefa@gmail.com>
2023-06-28 15:01:35 +0200

    Setup Nvchad 2 porting custom mappings, options and plugins
    I left out the following:
    
    * vim-tmux-navigator: That behavior built-in in Nvchad
    * SQLUtilities: Three are a few formatters in null-ls that provide
      formatting of SQL statements.

--- nvchad_config/custom/mappings.lua
+++ nvchad_config/custom/mappings.lua
@@ -1,7 +1,11 @@
+---@type MappingsTable
 local M = {}
 
 M.general = {
   n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    -- Muscle memory inherited from my old Vim config
     ["\\g"] = { "<cmd> Telescope grep_string <CR>", "search string under cursor" },
     [",gg"] = { "<cmd> Telescope live_grep <CR>", "git grep" },
     ["<space>b"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
@@ -10,6 +14,13 @@ M.general = {
     -- utilities
     ["<leader>y"] = { "\"+y" }, -- yank into system's clipboard
   },
+  i = {
+    ["<C-J>"] = {
+      "copilot#Accept('<CR>')",
+      "accept current Copilot suggestion",
+      opts = { silent = true, expr = true, replace_keycodes = false }
+    }, -- replaces the default <Tab> which is already taken
+  }
 }
 
 -- more keybinds!
