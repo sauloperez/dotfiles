8fdf55d1a4c9281fbc38cc1677a087a2b5b3935c
Pau Perez <saulopefa@gmail.com>
2023-06-29 11:19:59 +0200

    Install a lua rewrite of "christoomey/vim-tmux-navigator"
    I was wrong in
    https://github.com/sauloperez/dotfiles/pull/2/commits/f31cb3cdb98502b92e28cbd3f3da1008c1998876.
    This is still needed.
    
    With https://github.com/christoomey/vim-tmux-navigator though, I
    couldn't move from Neovim to a Tmux pane. I found the solution to make
    it work the Lazy package manager in
    https://www.reddit.com/r/neovim/comments/117ko6e/install_plugin_in_lazyvim/jiweb66/.

--- nvchad_config/custom/plugins.lua
+++ nvchad_config/custom/plugins.lua
@@ -3,90 +3,108 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
-
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
-
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
-
-  -- Tim Pope's realm
 	{
-    "tpope/vim-surround",
-    lazy = false,
-  },
-	{
-    'tpope/vim-fugitive',
-    cmd = { "Git", "G" },
-  },
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- Focus mode
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
+
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
+
+	{
+		"alexghergh/nvim-tmux-navigation",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+					last_active = "<C-\\>",
+					next = "<C-Space>",
+				},
+			})
+		end,
+	},
+
+	-- Tim Pope's realm
+	{
+		"tpope/vim-surround",
+		lazy = false,
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = { "Git", "G" },
+	},
+
+	-- Focus mode
 	-- Call TZAtaraxis for Goyo equivalent
-  {
-    "Pocco81/TrueZen.nvim",
-    cmd = { "TZAtaraxis", "TZMinimalist" },
-  },
-  -- Dims all but the portions of the code that you're editing
-  {
-    "folke/twilight.nvim",
-    cmd = { "Twilight"},
-  },
+	{
+		"Pocco81/TrueZen.nvim",
+		cmd = { "TZAtaraxis", "TZMinimalist" },
+	},
+	-- Dims all but the portions of the code that you're editing
+	{
+		"folke/twilight.nvim",
+		cmd = { "Twilight" },
+	},
 
-  {
-    "github/copilot.vim",
-    lazy = false,
-  },
+	{
+		"github/copilot.vim",
+		lazy = false,
+	},
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
