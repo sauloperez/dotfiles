319ba35f6cc276d81d026792ba3d0a0379e964c1
Pau Perez <saulopefa@gmail.com>
2023-06-29 12:01:17 +0200

    Configure Nvchad's statusline

--- nvchad_config/custom/chadrc.lua
+++ nvchad_config/custom/chadrc.lua
@@ -10,6 +10,13 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  statusline = {
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+  },
 }
 
 M.plugins = "custom.plugins"
