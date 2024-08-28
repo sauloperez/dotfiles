6494da050e6c28fc2756008ab7bc9ba6b36663d3
Pau Perez <saulopefa@gmail.com>
2024-08-28 08:10:58 +0200

    Move aliases and functions over to omz customizations
    All .zsh files are loaded by Omz.

---
+++ oh-my-zsh-custom/config.zsh
@@ -0,0 +1,26 @@
+# Put files in this folder to add your own custom functionality.
+# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
+#
+# Files in the custom/ directory will be:
+# - loaded automatically by the init script, in alphabetical order
+# - loaded last, after all built-ins in the lib/ directory, to override them
+# - ignored by git by default
+#
+# Example: add custom/shortcuts.zsh for shortcuts to your local projects
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+# All my personal configs are here so as not to overwrite the default Oh My Zsh
+# zshrc file. Keeping a dotfiles version in sync by hand with theirs it's too hard.
+ZSH_THEME="dracula"
+
+# Common junk
+# [[ -s "$HOME/.commonrc" ]] && source "$HOME/.commonrc"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git vi-mode)
