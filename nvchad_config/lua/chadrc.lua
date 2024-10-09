a2d5bc86984f299e80265733d5954f7f35531b4d
Pau Perez <saulopefa@gmail.com>
2024-10-09 18:11:31 +0300

    Fix import

--- nvchad_config/lua/chadrc.lua
+++ nvchad_config/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require "highlights"
 
 M.ui = {
   theme = "tokyonight",
