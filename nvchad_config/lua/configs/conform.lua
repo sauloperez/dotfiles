aff3ca88a6b06b09d3614779077af0360b1c2129
Pau Perez <saulopefa@gmail.com>
2025-04-01 17:32:35 +0200

    Format with Ruff on save

--- nvchad_config/lua/configs/conform.lua
+++ nvchad_config/lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
   formatters_by_ft = {
+    python = {
+      "ruff_fix", -- To fix lint errors. (ruff with argument --fix)
+      "ruff_format", -- To run the formatter. (ruff with argument format)
+      "ruff_organize_imports",
+    },
     lua = { "stylua" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
