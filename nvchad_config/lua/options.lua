4495f7ede402df9223a0a629ef43d5c91f59a261
Pau Perez <saulopefa@gmail.com>
2025-04-01 17:32:11 +0200

    Highlight yanked text

--- nvchad_config/lua/options.lua
+++ nvchad_config/lua/options.lua
@@ -20,3 +20,11 @@ autocmd("LspAttach", {
 	end,
 })
 
+-- Highlight when yanking (copying) text
+autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
