ec24fc0010b250c546f9d5a90a389c01389f031f
Pau Perez <saulopefa@gmail.com>
2025-04-10 11:09:06 +0200

    Port plugins from NvChad

---
+++ config/nvim/lua/plugins/core.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    opts = {
+      disable_when_zoomed = true, -- defaults to false
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      },
+    },
+  },
+  {
+    "chrisbra/matchit",
+    lazy = false,
+  },
+}
