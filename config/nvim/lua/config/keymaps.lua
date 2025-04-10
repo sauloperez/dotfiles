0e824964af9d54dc185497d74870f05689c0379c
Pau Perez <saulopefa@gmail.com>
2025-04-10 10:04:57 +0200

    Port yank-to-clipboard keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Yank to clipboard
+vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
