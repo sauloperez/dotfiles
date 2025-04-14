a5c281dde5b93623806be81cbd0d4bdbc385b4d0
Pau Perez <saulopefa@gmail.com>
2025-04-14 10:52:45 +0200

    Remove format_on_save as it's set by LazyVim

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -21,12 +21,6 @@ local options = {
     },
     lua = { "stylua" },
   },
-
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
 }
 
 return {
