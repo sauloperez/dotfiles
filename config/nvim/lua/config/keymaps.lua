44c687f511ba67ae3960a19d00a2cbb0e471a956
Pau Perez <saulopefa@gmail.com>
2025-04-14 09:24:17 +0200

    Fix tmux navigation keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,13 @@
 
 -- Yank to clipboard
 vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
+
+-- Keymaps matching "alexghergh/nvim-tmux-navigation" plugin.
+-- LazyVim overrides these keymaps so adding them in the plugin spec didn't work.
+-- See: https://www.reddit.com/r/neovim/comments/14yer8w/comment/jrsax4w/
+vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
+vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -4,14 +4,6 @@ return {
     event = "VeryLazy",
     opts = {
       disable_when_zoomed = true, -- defaults to false
-      keybindings = {
-        left = "<C-h>",
-        down = "<C-j>",
-        up = "<C-k>",
-        right = "<C-l>",
-        last_active = "<C-\\>",
-        next = "<C-Space>",
-      },
     },
   },
   {
