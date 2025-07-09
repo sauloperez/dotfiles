5d9dc3d2b23725adb2a3a1c56b527a999604c6a9
pau <saulopefa@gmail.com>
2025-07-09 15:02:45 +0200

    Re-map ctrl to caps lock programmatically

--- config/ohmyzsh/config.zsh
+++ config/ohmyzsh/config.zsh
@@ -12,3 +12,6 @@ eval "$(nodenv init -)"
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init - zsh)"
+
+# Map ctrl to caps lock
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
