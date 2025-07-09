1679f27ff8872a6ff0dc41789b693b8b15a51241
Pau Perez <saulopefa@gmail.com>
2025-07-09 11:40:12 +0200

    Remove `copy` alias in favor of omz's clipcopy/clippaste

--- config/ohmyzsh/aliases.zsh
+++ config/ohmyzsh/aliases.zsh
@@ -6,7 +6,6 @@ alias grep='GREP_COLOR="1;37;45" LANG=C grep --color=auto'
 alias h="omz_history"
 alias view="nvim -R"
 alias timestamp="date +%s"
-alias copy="xsel -ib"
 
 # Git aliases. Note that most of them depend on an alias already defined in
 # gitconfig.
